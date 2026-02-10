1631b7d8b6312bbacc53721bed8f3af8f4251ad8
haziqahmadshah <84791297+haziqahmadshah@users.noreply.github.com>
2026-02-11 00:00:52 +0530

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,24 @@
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyB0FpYJytVZuHjtHjL5-RDK1MlIEEi6K6c",
+  authDomain: "private-chat-4f4b9.firebaseapp.com",
+  projectId: "private-chat-4f4b9",
+  messagingSenderId: "641108436601",
+  appId: "1:641108436601:web:f67fdadc0d76bba11116a8"
+});
+
+const messaging = firebase.messaging();
+
+/*
+  🔔 Background notifications
+  — NO text
+  — NO images
+  — App name only
+*/
+messaging.onBackgroundMessage(() => {
+  self.registration.showNotification("💗 H & K", {
+    silent: false
+  });
+});
